dbd0fd73e9ce4e37805a632e12348c5f56d93bbc
fackumaretl-crypto <facku.maretl@gmail.com>
2025-12-01 16:32:11 -0300

    Update settings.py

--- parcial2-master/config/settings.py
+++ parcial2-master/config/settings.py
@@ -66,7 +66,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         conn_health_checks=True,
     )
